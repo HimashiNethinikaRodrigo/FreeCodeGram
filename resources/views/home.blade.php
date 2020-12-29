bb58d10e635c2f3707e2ed91f78fb33601a615eb
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-12-29 20:43:19 +0100

    add header image and header information

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,22 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img
+                src="https://github.com/HimashiNethinikaRodrigo/InstaLike-Application/blob/master/public/images/header_image.png?raw=true" alt="header image"
+                class="rounded-circle" height="150px">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>InstaLike</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
             </div>
         </div>
     </div>
+
+
 </div>
 @endsection
